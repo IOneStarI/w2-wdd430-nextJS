ceee4e558abba42ad46b463c3fa5ce2c2fca5fdf
Vlad Didkovskiy <vladgreen.vg@gmail.com>
2026-03-19 15:56:41 +0200

    w3 task

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,6 +1,12 @@
 import postgres from 'postgres';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const connectionString = process.env.POSTGRES_URL;
+
+if (!connectionString) {
+  throw new Error('POSTGRES_URL is not set');
+}
+
+const sql = postgres(connectionString, { ssl: 'require' });
 
 async function listInvoices() {
 	const data = await sql`
@@ -14,10 +20,6 @@ async function listInvoices() {
 }
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
   try {
   	return Response.json(await listInvoices());
   } catch (error) {
